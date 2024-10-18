48321638beda02948846988560b8f0f138c639b5
Robin Bertram <git@robin.bertram.id>
2024-10-18 15:33:12 +0200

    add polishedcrystal-nortc-3.0.0-beta

---
+++ roms/pokemon_hacks/polishedcrystal-nortc-3.0.0-beta-22d6f8e1/settings.asm
@@ -0,0 +1,145 @@
+; ------------------------------------------------------------------------------
+;                            Pokemon Polished Crystal
+;    get  polishedcrystal-nortc-3.0.0-beta-22d6f8e1.gbc from
+;    https://github.com/Rangi42/polishedcrystal/releases/tag/v3.0.0-beta
+;
+;    More info at https://github.com/Rangi42/polishedcrystal
+; ------------------------------------------------------------------------------
+; MIT License
+;
+; Copyright (c) 2024 Marc Robledo
+; Copyright (c) 2024 Robin Bertram
+;
+; Permission is hereby granted, free of charge, to any person obtaining a copy
+; of this software and associated documentation files (the "Software"), to deal
+; in the Software without restriction, including without limitation the rights
+; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+; copies of the Software, and to permit persons to whom the Software is
+; furnished to do so, subject to the following conditions:
+;
+; The above copyright notice and this permission notice shall be included in all
+; copies or substantial portions of the Software.
+;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+; SOFTWARE.
+; ------------------------------------------------------------------------------
+
+
+; CARTRIDGE TYPE AND ROM SIZE
+; ---------------------------
+; Usually, it's safe to keep the same original game's ROM type and size, since
+; RGBDS will automatically fix the ROM's header if we expand it for the
+; savegame.
+; Uncomment the following constants if you want to manually specify cartridge
+; type and/or size:
+; DEF CHANGE_CART_TYPE EQU CART_ROM_MBC5_RAM_BAT
+; DEF CHANGE_CART_SIZE EQU CART_ROM_2048KB ;128 banks
+
+
+
+; SRAM ORIGINAL SIZE
+; ------------------
+; Set to 1 if game's original SRAM is 32kb
+DEF SRAM_SIZE_32KB EQU 1
+
+
+
+; GAME BOOT OFFSET
+; ----------------
+; Put here the game's boot jp offset found in in 0:0101.
+; Usually $0150, but could be different depending on game.
+DEF GAME_BOOT_OFFSET EQU $0177
+
+
+
+; BANK 0 ROM FREE SPACE
+; ---------------------
+; We need ~60 bytes (~0x3c bytes) in bank 0 for our new subroutines:
+; - new boot: will read savegame from Flash ROM and write to SRAM during boot
+; - new save/erase: will copy SRAM to Flash ROM when saving game
+; - copying helper subroutine
+; Hopefully, there should be enough space at the end of bank 0.
+; If there is not enough space there, check $0070-$0100, some games didn't
+; store anything there.
+; In the worst scenario, you will need to carefully move some code/data to
+; other banks.
+DEF BANK0_FREE_SPACE EQU $3d35
+
+
+
+; RAM FREE SPACE
+; --------------
+; Bootleg's Flash ROM reading is locked when trying to write to it, so we need
+; to store and run our new subroutines in WRAM instead of ROM.
+; We need ~80 bytes (~0x50 bytes).
+; Check which WRAM sections are safe to write with a debugger.
+; If the game uses some compression or temporary section to store data, that
+; should be safe to use.
+; In the worst scenario, use shadow OAM space. It will just glitch sprites for
+; a single frame.
+; If it's a color-only game, $d000-$dfff is banked.
+; Therefore you have to add a WRAM_BANK_NUMBER to use this address space.
+; Additionaly - the Stack has to be in WRAM0 $c000-$cfff for this to work
+DEF WRAM_FREE_SPACE EQU $d562
+DEF WRAM_BANK_NUMBER EQU $5
+
+IF DEF(_BATTERYLESS)
+
+; NEW CODE LOCATION
+; -----------------
+; We need ~80 bytes (~0x50 bytes) to store our new battery-less save code.
+; As stated above, they will be copied from ROM to WRAM0 when trying to save.
+DEF BATTERYLESS_CODE_BANK EQU $71
+DEF BATTERYLESS_CODE_OFFSET EQU $4000
+
+
+
+; GAME ENGINE'S CURRENT BANK BACKUP LOCATION
+; ------------------------------------------
+; Games usually store the current ROM bank number in RAM or HRAM, so they can
+; restore the correct bank when switching back from VBlank.
+; We will reuse that byte when switching to our battery-less code bank and,
+; afterwards, so we can restore to the previous bank.
+DEF GAME_ENGINE_CURRENT_BANK_OFFSET EQU $ff93
+
+
+
+; SAVEGAME LOCATION IN FLASH ROM
+; ------------------------------
+; Starting Flash ROM bank where savegame data will be saved.
+; IMPORTANT: It must be an entire 64kb flashable block!
+; If the game has not a free 64kb block, just use a bank bigger than the
+; original ROM and RGBDS will expand the ROM and fix the header automatically.
+DEF BANK_FLASH_DATA EQU $74
+
+
+
+; EMBED CUSTOM SAVEGAME
+; ---------------------
+; Just place a sav file next to the input ROM - with the extension .sav instead of .gbc
+; If a sav file is present, it will be included into the batteryless ROM.
+
+
+
+; ORIGINAL SAVE SUBROUTINE
+; ------------------------
+; We need to find the original game's saving subroutine and hook our new code
+; afterwards.
+SECTION "save_hook: overwrite code at the end of SaveCurrentVersion", ROMX[$4cc5], BANK[$05]
+	; jp CloseSRAM; $2a60
+    jp save_sram_hook
+
+SECTION "Save SRAM hook", ROM0[$3ff9]
+save_sram_hook:
+    ;original code
+    call $2a60
+    ;new code
+    call save_sram_to_flash
+    ret
+ENDC
+
